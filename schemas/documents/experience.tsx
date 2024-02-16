e414fb15371a591195bccb34736bede5bddb21ba
Yoni Touboul <yonitouboul@hotmail.com>
2024-02-16 12:27:21 +0100

    Added intro description, title & ambient sound

--- schemas/documents/experience.tsx
+++ schemas/documents/experience.tsx
@@ -1,10 +1,22 @@
 import { useMemo } from "react";
 import createImageUrlBuilder from "@sanity/image-url";
 
-import { ImageValue, ObjectInputProps, ObjectSchemaType, defineField, defineType, useClient } from "sanity";
+import {
+	ArrayOfPrimitivesInputProps,
+	ArraySchemaType,
+	ImageValue,
+	ObjectInputProps,
+	ObjectSchemaType,
+	defineArrayMember,
+	defineField,
+	defineType,
+	useClient,
+} from "sanity";
 import { HotspotTooltipProps } from "sanity-plugin-hotspot-array";
 import styled from "styled-components";
 import { ImageIcon } from "@sanity/icons";
+import { fontSizeMultiplier } from "../../utils/fontSizeMultiplier";
+import { fontEnum } from "../types/fontEnum";
 
 const HotspotPreview = ({ value, renderPreview, schemaType }: HotspotTooltipProps): JSX.Element => {
 	return (
@@ -17,6 +29,19 @@ const HotspotPreview = ({ value, renderPreview, schemaType }: HotspotTooltipProp
 		</>
 	);
 };
+
+const SectionTitlePreview = styled.h2`
+	font-size: 4rem;
+	margin: 0;
+	font-weight: unset;
+	text-align: center;
+`;
+
+const PreviewLetter = styled.span<{ fontFamily: fontEnum }>`
+	text-transform: uppercase;
+	font-family: ${(props) => props.fontFamily};
+	font-size: ${(props) => `${fontSizeMultiplier[props.fontFamily]}em`};
+`;
 
 const Layer = styled.div<{ $url: string }>`
 	width: 100%;
@@ -48,6 +73,12 @@ const PreviewImage = (props: ObjectInputProps<ImageValue, ObjectSchemaType>): JS
 	);
 };
 
+interface TitleLetter {
+	letter: string;
+	fontFamily: fontEnum;
+	_key: string;
+}
+
 export default defineType({
 	name: "experience",
 	title: "Expérience",
@@ -63,6 +94,56 @@ export default defineType({
 				hotspot: false,
 			},
 			validation: (rule) => rule.required().assetRequired(),
+		}),
+		defineField({
+			name: "title",
+			type: "array",
+			title: "Titre (Fenêtre introductive)",
+			components: {
+				input: (props: ArrayOfPrimitivesInputProps<string | number | boolean, ArraySchemaType<unknown>>) => {
+					const { value, renderDefault } = props;
+					return (
+						<>
+							<SectionTitlePreview>
+								{value &&
+									value.map((l) => {
+										const letter = l as unknown as TitleLetter;
+										return (
+											// eslint-disable-next-line no-underscore-dangle
+											<PreviewLetter fontFamily={letter.fontFamily} key={letter._key}>
+												{letter.letter}
+											</PreviewLetter>
+										);
+									})}
+							</SectionTitlePreview>
+							{renderDefault(props)}
+						</>
+					);
+				},
+			},
+			of: [
+				defineArrayMember({
+					name: "content",
+					title: "Contenu",
+					type: "content",
+				}),
+			],
+			validation: (Rule) => Rule.required().min(1),
+		}),
+		defineField({
+			title: "Description (Fenêtre Introductive)",
+			name: "description",
+			type: "string",
+			validation: (Rule) => Rule.required(),
+		}),
+		defineField({
+			name: "sound",
+			type: "file",
+			title: "Fichier audio",
+			options: {
+				accept: "audio/*",
+			},
+			validation: (Rule) => Rule.required().assetRequired(),
 		}),
 		defineField({
 			type: "image",
